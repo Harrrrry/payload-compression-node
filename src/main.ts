c36792b0f476fed153179813d9275f92e596e67c
harendra.tomer <harendra.tomer@fullbay.com>
2024-09-06 17:01:59 +0530

    Update cors lib

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS with the necessary configurations
-  app.enableCors({
-    origin: 'https://mellow-frangipane-4f17ec.netlify.app/',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization, Content-Encoding',
-  });
+  app.use(
+    cors({
+      origin: 'https://mellow-frangipane-4f17ec.netlify.app',
+      methods: 'GET,POST,PUT,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type, Authorization, Content-Encoding',
+    }));
 
   // Increase payload size limit
   app.use(express.json({ limit: '50mb' }));
